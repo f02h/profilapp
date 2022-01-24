6d1cf6b483ead705ca6b50563265d374fb859e9a
f02h <Preeleesje42>
2022-01-24 21:29:23 +0100

    1

--- app.py
+++ app.py
@@ -140,12 +140,13 @@ profilList = dict(res)
 
 
 n = tk.StringVar()
+n.trace("w", callback)
 monthchoosen = ttk.Combobox(tab2, width=27,
                             textvariable=n)
 # Adding combobox drop down list
 monthchoosen['values'] = list(profilList.values())
 monthchoosen.grid(column=1, row=15)
-monthchoosen.trace("w", callback)
+
 
 Button(tab2, text='Submit', command=submitForm, width=20,bg='brown',fg='white').place(x=100,y=100)
 
