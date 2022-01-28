035185f157c18fc07853c488d26623450bfccf4a
f02h <staric.david@gmail.com>
2022-01-28 21:55:33 +0100

    1

--- main2.py
+++ main2.py
@@ -654,7 +654,7 @@ moveStepperInput = Entry(canvas_tab3, font=text_font, width=10)
 moveStepperInput.grid(column=1, row=2)
 
 stepperButton = Button(canvas_tab3, text='Premakni stepper', command=moveStepper, width=20,bg='brown',fg='white',font=('Arial', '20')).grid(column=0, row=3)
-tk.Label(canvas_tab3, text='     \n   ').grid(column=0,row=4)
+#tk.Label(canvas_tab3, text='     \n   ').grid(column=0,row=4)
 
 tk.Label(canvas_tab3, text="Izberi spindle: ", font=text_font,anchor='w', width=15).grid(column=0, row=5,pady=(15,0))
 spindlechoosen = ttk.Combobox(canvas_tab3, width=5,textvariable=n,font=('Arial', '30'))
@@ -671,7 +671,7 @@ tk.Label(canvas_tab3, text='     \n   ').grid(column=0,row=8)
 
 toggle_button3 = Button(canvas_tab3,text="OFF ALL", width=10, command=toggleAllOff, bg='brown',fg='white',font=('Arial', '20')).grid(column=0, row=9)
 toggle_button4 = Button(canvas_tab3,text="ON ALL", width=10, command=toggleAllOn, bg='green',font=('Arial', '20')).grid(column=1, row=9)
-#tk.Label(canvas_tab3, text='     \n   ').grid(column=0,row=10)
+tk.Label(canvas_tab3, text='     \n   ').grid(column=0,row=10)
 
 toolButton1 = Button(tool_tab3,text="T1", width=10, command=lambda :changeTool(1,'LEFT'), bg='green',font=('Arial', '20')).grid(column=0, row=0)
 toolButton2 = Button(tool_tab3,text="T2", width=10, command=lambda :changeTool(2,'RIGHT'), bg='green',font=('Arial', '20')).grid(column=1, row=0)
