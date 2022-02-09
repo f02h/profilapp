7231a4d3f2b431f2e7c18deb9c7de03f73b9a039
f02h <staric.david@gmail.com>
2022-02-09 20:39:40 +0100

    1

--- main2.py
+++ main2.py
@@ -12,10 +12,10 @@ import os
 
 USB_PORT = "/dev/ttyACM0"
 USB_PORT_FEEDER = "/dev/ttyUSB0"
-#usb = serial.Serial(USB_PORT, 115200)
-#usbf = serial.Serial(USB_PORT_FEEDER, 115200)
-usb = 0
-usbf = 0
+usb = serial.Serial(USB_PORT, 115200)
+usbf = serial.Serial(USB_PORT_FEEDER, 115200)
+#usb = 0
+#usbf = 0
 path = os.path.dirname(os.path.abspath(__file__))
 db = os.path.join(path, 'todo.db')
 
