ee831b843f0316e018943e8979e49da7e863ee6c
f02h <Preeleesje42>
2023-08-11 20:02:29 +0200

    1

--- testing/json-serial.py
+++ testing/json-serial.py
@@ -5,8 +5,11 @@ serialPort = serial.Serial(
     port="COM3", baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
 )
 serialString = ""  # Used to hold data coming over UART
+
+data = {"A": "test5"}
+
+serialPort.write(json.dumps(data).encode())
 while 1:
-    # Wait until there is data waiting in the serial buffer
     if serialPort.in_waiting > 0:
 
         # Read data out of the buffer until a carraige return / new line is found
@@ -15,8 +18,11 @@ while 1:
         # Print the contents of the serial data
         try:
            # print(serialString.decode("Ascii"))
+
+
             mystring = json.loads(str(serialString.decode("Ascii")).strip())
             print(mystring)
-            print(mystring["sensor"])
+            print(mystring["R"])
         except:
+            print("fail")
             pass
