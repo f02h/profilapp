84409ed9bd8824cbb563d9995584144b5a5d4cc1
f02h <staric.david@gmail.com>
2023-08-14 19:53:59 +0200

    1

--- pomik_profila.ino
+++ pomik_profila.ino
@@ -1,8 +1,10 @@
 #include <Wire.h>
-#include <GPIO.h>
+#include <ESP8266WiFi.h>
+
+//#include <GPIO.h>
 
 #include <AccelStepper.h>
-#include <SpeedyStepper.h>
+//#include <SpeedyStepper.h>
 #include <ArduinoJson.h>
 
 int stepsPerMM = 160;
@@ -10,52 +12,54 @@ int stepsPerMM = 160;
 // init motors
 //GPIO<BOARD::D38> startSenzorMali;
 //GPIO<BOARD::D37> startSenzorVeliki;
-int startSenzorMali = 38;
-int startSenzorVeliki = 37;
+int startSenzorMali = D6;
+int startSenzorVeliki = D5;
+int hommingSenzor1 = D7;
 
-
+int directionPin1 = D2;
+int stepPin1 = D1;
+/*
 GPIO<BOARD::D39> hommingSenzor1;
 GPIO<BOARD::D53> directionPin1;
-GPIO<BOARD::D52> stepPin1;
-AccelStepper stepper1(AccelStepper::DRIVER, 52, 53);
+GPIO<BOARD::D52> stepPin1;*/
+AccelStepper stepper1(AccelStepper::DRIVER, stepPin1, directionPin1);
 
 // 4385mm / 180,25 per rev / 4000 steps
 int maxHod = 97309;
 
+bool changeLength = false;
+
+
+
 void setup() {
   Serial.begin(115200);
-  //startSenzorMali.input();
-  //startSenzorVeliki.input();
+  while (!Serial) continue;
+
   pinMode(startSenzorMali, INPUT);
   pinMode(startSenzorVeliki, INPUT);
+  pinMode(stepPin1, OUTPUT);
+  pinMode(directionPin1, OUTPUT);
 
- 
-  stepPin1.output();
-  stepPin1 = LOW;
-  directionPin1.output();
-  directionPin1 = HIGH;
+  digitalWrite(stepPin1, LOW);
+  digitalWrite(directionPin1, HIGH);
 
-  stepper1.setMaxSpeed(500000);
-  stepper1.setAcceleration(5000.0);
+  stepper1.setMaxSpeed(5000000);
+  stepper1.setAcceleration(10000.0);
 
-    homming();
+  homming();
 
   stepper1.setCurrentPosition(0);
 
-  //180,25
+  //moveRev(24000,0);
 
-    moveRev(12000,0);
- /*
-  stepper1.moveTo(-97309);
-  stepper1.runToPosition();*/  
 }
 
 
 
-void loop() 
+void loop()
 {
 
-    
+    changeLength = false;
 
     if (Serial.available()) {  // check for incoming serial data
 
@@ -65,71 +69,102 @@ void loop()
        doc3["M"] = 0;
        serializeJson(doc3, Serial);
        Serial.println();*/
-      
-      String command = Serial.readString(); 
+
+      String command = Serial.readString();
       StaticJsonDocument<500> doc;
-      int str_len = command.length() + 3; 
+      int str_len = command.length() + 3;
       char char_array[str_len];
       command.toCharArray(char_array, str_len);
 
       DeserializationError error = deserializeJson(doc, char_array);
-      
+
       // Test if parsing succeeds.
       if (error) {
         Serial.print(F("deserializeJson() failed: "));
         Serial.println(error.f_str());
         return;
-      }
-    
-      // Fetch values.
-      //
-      // Most of the time, you can rely on the implicit casts.
-      // In other case, you can do doc["time"].as<long>();
-      String action = doc["A"];
-      int profile = doc["P"];
-      long int moveStep = doc["M"];
-      int absMove = doc["M2"];
+      } else {
 
-      
-/*
-      StaticJsonDocument<500> doc3;
-       doc3["status"] = "done";
-       doc3["A"] = action;
-       doc3["M"] = moveStep;
-       doc3["F"] = firstMove;
-       doc3["com"] = command;
-       serializeJson(doc3, Serial);
-       Serial.println();
-  */    
+        // Fetch values.
+        //
+        // Most of the time, you can rely on the implicit casts.
+        // In other case, you can do doc["time"].as<long>();
+        String action = doc["A"];
+        int profile = doc["P"];
+        long int moveStep = doc["M"];
+        int absMove = doc["M2"];
+
       // read command from serial port
-      if (action == "moveFwdF") {
-         while(!waitForProfile(profile)) {}  
-         moveFwd(moveStep, absMove);
-         StaticJsonDocument<200> doc2;
-         doc2["status"] = "done";
-         serializeJson(doc2, Serial);
-         Serial.println();
-      } else if (action == "moveFwd") {
-         moveFwd(moveStep, absMove);
-         StaticJsonDocument<200> doc2;
-         doc2["status"] = "done";
-         serializeJson(doc2, Serial);
-         Serial.println();
-      } else if (action == "moveRev") {  // turn on LED
-         moveRev(moveStep, absMove);
-         StaticJsonDocument<200> doc2;
-         doc2["status"] = "done";
-         serializeJson(doc2, Serial);
-         Serial.println();
-      } else if (action == "home") {  // turn on LED
-         homming();
-         StaticJsonDocument<200> doc2;
-         doc2["status"] = "done";
-         serializeJson(doc2, Serial);
-         Serial.println();
+
+        if (action == "waitForProfile") {
+          /*while (!waitForProfile(profile) && !changeLength) {
+            if (Serial.available()) {  // check for incoming serial data
+              String command = Serial.readString();
+              StaticJsonDocument<500> doc;
+              int str_len = command.length() + 3;
+              char char_array[str_len];
+              command.toCharArray(char_array, str_len);
+
+              DeserializationError error = deserializeJson(doc, char_array);
+
+              if (error) {
+                Serial.print(F("deserializeJson() failed: "));
+                Serial.println(error.f_str());
+                return;
+              } else {
+                if (doc["A"] == "changeLength") {
+                  changeLength = true;
+                }
+              }
+            }
+
+            if (!changeLength) {
+              DynamicJsonDocument doc2(1024);
+                  doc2["status"] = "waitingForProfile";
+                  serializeJson(doc2, Serial);
+                  Serial.println();
+            }
+          } */
+
+          if (waitForProfile(profile)) {
+            DynamicJsonDocument doc2(1024);
+            doc2["status"] = "done";
+            serializeJson(doc2, Serial);
+            Serial.println();
+          } else {
+            DynamicJsonDocument doc2(1024);
+            doc2["status"] = "waiting";
+            serializeJson(doc2, Serial);
+            Serial.println();
+          }
+
+        } else if (action == "moveFwdF") {
+          moveFwd(moveStep, absMove);
+          DynamicJsonDocument doc2(1024);
+          doc2["status"] = "done";
+          serializeJson(doc2, Serial);
+          Serial.println();
+        } else if (action == "moveFwd") {
+          moveFwd(moveStep, absMove);
+          DynamicJsonDocument doc2(1024);
+          doc2["status"] = "done";
+          serializeJson(doc2, Serial);
+          Serial.println();
+        } else if (action == "moveRev") {  // turn on LED
+          moveRev(moveStep, absMove);
+          DynamicJsonDocument doc2(1024);
+          doc2["status"] = "done";
+          serializeJson(doc2, Serial);
+          Serial.println();
+        } else if (action == "home") {  // turn on LED
+          homming();
+          DynamicJsonDocument doc2(1024);
+          doc2["status"] = "done";
+          serializeJson(doc2, Serial);
+          Serial.println();
+        }
       }
 
-      
    }
 
 }
@@ -143,61 +178,43 @@ boolean waitForProfile(int profileSize) {
   if (senzorMali) {
     senzorMali = digitalRead(startSenzorMali);
   }
-  
-  
-/*
-  Serial.println();
-  
-  if (startSenzorMali) {
-    Serial.println("mali profil");
-  }
 
-  if (startSenzorVeliki) {
-     Serial.println("veliki profil");
-  }
-
-  Serial.println();
-
-  return false;
-  
-*/
-  
   if (profileSize == 1) {
     if (senzorMali == HIGH  && senzorVeliki == LOW) {
       return true;
     }
-  } 
-  
+  }
+
   if (profileSize == 2) {
     if (senzorMali == HIGH && senzorVeliki == HIGH) {
         return true;
     }
-  } 
-  
+  }
+
   return false;
 }
 
 
 
-boolean moveFwd(long int moveSteps, int absMove) {  
+boolean moveFwd(long int moveSteps, int absMove) {
   if (absMove == 1) {
-    stepper1.moveTo(moveSteps);  
+    stepper1.moveTo(moveSteps);
   } else {
     stepper1.move(moveSteps);
   }
-  
+
   stepper1.runToPosition();
   return true;
-  
+
 }
 
 boolean moveRev(long int moveSteps, int absMove) {
   if (absMove == 1) {
-    stepper1.moveTo(moveSteps * -1);  
+    stepper1.moveTo(moveSteps * -1);
   } else {
     stepper1.move(moveSteps * -1);
   }
-  
+
   stepper1.runToPosition();
   return true;
 }
@@ -214,15 +231,17 @@ boolean homming() {
 
 boolean homming1() {
 
-  directionPin1 = HIGH;
+  //directionPin1 = HIGH;
+  digitalWrite(directionPin1, HIGH);
 
-  if (hommingSenzor1 == LOW)
+  int hommingSenzorState = digitalRead(hommingSenzor1);
+  if (hommingSenzorState == LOW)
   {
     boolean limitSwitchFlag = false;
     while (1)
     {
-
-      if (hommingSenzor1 == HIGH)
+      hommingSenzorState = digitalRead(hommingSenzor1);
+      if (hommingSenzorState == HIGH)
       {
         limitSwitchFlag = true;
         break;
@@ -237,13 +256,16 @@ boolean homming1() {
       return (false);
   }
 
-  directionPin1 = LOW;
-  if (hommingSenzor1 == HIGH)
+  digitalWrite(directionPin1, LOW);
+  //directionPin1 = LOW;
+  hommingSenzorState = digitalRead(hommingSenzor1);
+  if (hommingSenzorState == HIGH)
   {
     boolean limitSwitchFlag = false;
     while (1)
     {
-      if (hommingSenzor1 == LOW)
+      hommingSenzorState = digitalRead(hommingSenzor1);
+      if (hommingSenzorState == LOW)
       {
         limitSwitchFlag = true;
         break;
@@ -257,17 +279,17 @@ boolean homming1() {
       return (false);
   }
 
-  directionPin1 = LOW;
-
+  digitalWrite(directionPin1, LOW);
+  //directionPin1 = LOW;
   return true;
 }
 
 void moveOneStep1() {
     //directionPin = LOW;
-
-    stepPin1 = HIGH;
+    digitalWrite(stepPin1, HIGH);
+    //stepPin1 = HIGH;
     delayMicroseconds(40);
-    stepPin1 = LOW;
+    digitalWrite(stepPin1, LOW);
+    //stepPin1 = LOW;
     delayMicroseconds(40);
- 
 }
