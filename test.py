1846458b35de2a1a4148718c920e5a9f2ad42612
f02h <staric.david@gmail.com>
2023-09-04 20:29:56 +0200

    1

--- test.py
+++ test.py
@@ -5,64 +5,64 @@ import sqlite3
 from tkinter import *
 from tkinter import ttk
 from tkinter.scrolledtext import ScrolledText
-import os
+import os, time
+from threading import Thread
 
+USB_PORT_FEEDER = "/dev/ttyUSB0"
+#usbf = serial.Serial(USB_PORT_FEEDER, 115200)
+usbf = serial.Serial(
+    port=USB_PORT_FEEDER, baudrate=115200, bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE
+)
 
+def moveFeeder(dir, step, abs=0, firstMove=0):
 
+   idProfil = 1
 
-USB_PORT = "/dev/ttyACM0"
-USB_PORT_FEEDER = "/dev/ttyUSB1"
-usb = serial.Serial(USB_PORT, 115200)
-usbf = serial.Serial(USB_PORT_FEEDER, 115200)
-#usb = 0
-#usbf = 0
-path = os.path.dirname(os.path.abspath(__file__))
-db = os.path.join(path, 'todo.db')
+   # 44.44
+   data = {
+      "A": str(dir),
+      "M": str(int(step * 44.44)),
+      "M2": abs,
+      "P": idProfil,
+      "F": firstMove
+   }
 
-conn = sqlite3.connect(db)
-c = conn.cursor()
+   usbf.write(json.dumps(data).encode())
+   hearv = hearJsonf()
+   # print(hearv)
+   return hearv["status"]
 
-settingsList = dict()
-currentSetting = None
-
-stepperList = {1:0,2:0,3:0,4:0,5:0,6:0,7:0}
-spindleList = {1:0,2:0,3:0,4:0,5:0,6:0,7:0}
-
-sensorToDrill = 0
 
 def hearJsonf():
-    msg = usbf.read_until()# read until a new line
-    mystring = json.loads(str(msg.decode("utf-8")).strip())
-    return mystring
+   # msg = usbf.read_until()# read until a new line
+   # mystring = json.loads(str(msg.decode("Ascii")).strip())
+   # return mystring
 
-def moveFeeder(dir, step):
+   while 1:
+      if usbf.in_waiting > 0:
 
-    data = {
-        "A": str(dir),
-        "M": int(float(step)) *160,
-        "P": 1
-    }
+         # Read data out of the buffer until a carraige return / new line is found
+         serialString = usbf.readline()
 
-    usbf.write(json.dumps(data).encode())
-    hearv = hearJsonf()
-    """print(hearv)
-    if str(hearv["status"]).strip() == "done":
-        #cut.config(state=ACTIVE, bg='green')
-    else:
-        #cut.config(state=ACTIVE, bg='red')
-    label.config(text=str(hearv))
-"""
+         # Print the contents of the serial data
+         try:
+            # print(serialString.decode("Ascii"))
+
+            mystring = json.loads(str(serialString.decode("Ascii")).strip())
+            # print(mystring)
+            return mystring
+         except:
+            print("fail")
+            pass
 
 
 def homeFeeder():
-    data = {
-        "A": "home"
-    }
+   data = {
+      "A": "home"
+   }
 
-    usbf.write(json.dumps(data).encode())
-    hearv = hearJsonf()
-    if str(hearv["status"]).strip() == "done":
-        print('done')
-    else:
-        print('fail')
+   usbf.write(json.dumps(data).encode())
+   hearv = hearJsonf()
 
+homeFeeder()
+tmpStatus = moveFeeder("moveRev", float(200), 1, 1)
