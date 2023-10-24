3b6fc00ffb857af468d9a57bbe7ce35654b00ffa
f02h <staric.david@gmail.com>
2023-10-24 20:36:26 +0200

    1

--- main2.py
+++ main2.py
@@ -905,6 +905,8 @@ drill = tk.Button(tab1,text="Vrtaj",font=text_font,bg="green",command=executeDri
 exst = tk.Button(tab1,text="T",font=text_font,bg="green",command=extensionE)\
     .grid(column=2,columnspan=2,sticky=W+E,row=5,padx=30,pady=30)
 
+exst2 = tk.Button(tab1,text="T",font=text_font,bg="green",command=extensionF)\
+    .grid(column=4,columnspan=2,sticky=W+E,row=5,padx=30,pady=30)
 
 #cut = tk.Button(tab1,text="Žaga",font=text_font,bg="green",command=cut)\
 #    .grid(column=2,columnspan=2,sticky=W+E,row=5,padx=30,pady=30)
