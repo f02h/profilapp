976e7dd164ff169e31ceece24790f42939f3cb56
f02h <staric.david@gmail.com>
2023-10-26 21:44:15 +0200

    1

--- main2.py
+++ main2.py
@@ -514,7 +514,7 @@ def initJobs():
     res = c.execute("SELECT length,qty,idProfile,loader,qtyD,done,id FROM job WHERE done != 1").fetchall()
 
     tk.Label(vrtalkaDList, text="Dolžina", font=etext_font, anchor='w', width=10).grid(row=2, column=0)
-    tk.Label(vrtalkaDList, text="Profil", font=etext_font, anchor='w', width=35).grid(row=2, column=1)
+    tk.Label(vrtalkaDList, text="Profil", font=etext_font, anchor='w', width=25).grid(row=2, column=1)
     tk.Label(vrtalkaDList, text="Količina", font=etext_font, anchor='w', width=10).grid(row=2, column=2)
     tk.Label(vrtalkaDList, text="Ostane", font=etext_font, anchor='w', width=10).grid(row=2, column=3)
     tk.Label(vrtalkaDList, text="Zaključeno", font=etext_font, anchor='w', width=10).grid(row=2, column=4)
@@ -544,7 +544,7 @@ def initJobs():
         rowQtyD = row[4]
         rowDone = row[5]
         tk.Label(vrtalkaDList, text=rowLength, font=etext_font,anchor='w', width=10).grid(row=i, column=0)
-        tk.Label(vrtalkaDList, text=rowProfile, font=etext_font,anchor='w', width=35).grid(row=i, column=1)
+        tk.Label(vrtalkaDList, text=rowProfile, font=etext_font,anchor='w', width=25).grid(row=i, column=1)
         tk.Label(vrtalkaDList, text=rowQty, font=etext_font,anchor='w', width=10).grid(row=i, column=2)
         tk.Label(vrtalkaDList, text=rowQtyD, font=etext_font,anchor='w', width=10).grid(row=i, column=3)
         tk.Label(vrtalkaDList, text=rowDone, font=etext_font,anchor='w', width=10).grid(row=i, column=4)
@@ -557,8 +557,8 @@ def initJobs():
 def ctrlDeleteJob(idJob):
     sql = 'DELETE FROM job WHERE id=?'
     cur = conn.cursor()
-    cur.execute(sql, (idJob,))
-    conn.commit()
+    #cur.execute(sql, (idJob,))
+    #conn.commit()
 
     initJobs()
 
