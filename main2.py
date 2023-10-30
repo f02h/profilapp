871858081d9e627387f3967da781ecce7e307f10
f02h <staric.david@gmail.com>
2023-10-30 21:03:24 +0100

    1

--- main2.py
+++ main2.py
@@ -11,8 +11,8 @@ from PIL import Image,ImageTk
 
 
 USB_PORT = "/dev/ttyACM0"
-USB_PORT_FEEDER = "/dev/ttyUSB1"
-USB_PORT_LOADER = "/dev/ttyUSB0"
+USB_PORT_FEEDER = "/dev/ttyUSB0"
+USB_PORT_LOADER = "/dev/ttyUSB1"
 usb = serial.Serial(USB_PORT, 115200)
 #usbf = serial.Serial(USB_PORT_FEEDER, 115200)
 usbf = serial.Serial(
