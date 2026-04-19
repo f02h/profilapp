e5a6e4d0a1276d9e3dd2816a3d36bc4826cad38f
star1c <staric.david@gmail.com>
2026-04-19 21:29:01 +0200

    1

--- main2.py
+++ main2.py
@@ -1540,7 +1540,7 @@ def runCycle():
 
                 if plehekOffsetZadaj != 0:
 
-                    moveToPlehekZadaj = cut - plehekZadajFromStart - plehekOffsetZadaj
+                    moveToPlehekZadaj =  0 - plehekOffsetZadaj
 
                     print("Lukna plehek zadaj: " + str(moveToPlehekZadaj))
                     tmpStatus = moveFeeder("moveFwd", moveToPlehekZadaj,1)
